fc0bc8c500b73a7d4f739606c64207ae0c91b323
amishasalampuria <91007698+amishasalampuria@users.noreply.github.com>
2024-10-08 19:15:50 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-   const url = "http://localhost:4000"
+   // const url = "http://localhost:4000"
+   const url ='https://food-delivery-backend-unx5.onrender.com'
   return (
     <div>
         <ToastContainer/>
